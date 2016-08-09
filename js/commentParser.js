574aa03860f2d7af0d87ccd88d0ac84011816e35
myles <myles@Myles-OConnor-iMac-27.local>
2016-08-09 11:41:23 -0700

    Passing content of file instead of path, tags are being parsed

--- js/commentParser.js
+++ js/commentParser.js
@@ -1,10 +1,13 @@
 var parse = require('comment-parser'),
+    fs = require('fs'),
+    path = require('path'),
     date = new Date().toDateString(),
     time = new Date().toLocaleTimeString(),
-    test;
+    test,
+    content = fs.readFileSync(path.join(__dirname, '../scss/theme/_footer.scss'), 'utf8');
 
 console.log(`${date}, ${time} - Parsing Scss comments`);
 
-test = parse('../scss/theme/_footer.scss');
+test = parse(content);
 
 console.log(test);
