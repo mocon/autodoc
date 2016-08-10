3386ac722c61dea739e59e4056f8c050da6cadc9
myles <myles@Myles-OConnor-iMac-27.local>
2016-08-09 17:52:18 -0700

    Extracting comments from all .scss files, and concatenating them together into formatted JSON of comment tags

---
+++ commentParser.js
@@ -0,0 +1,39 @@
+var parse = require('comment-parser'),
+    recursive = require('recursive-readdir'),
+    fs = require('fs'),
+    path = require('path'),
+    date = new Date().toDateString(),
+    time = new Date().toLocaleTimeString(),
+    commentTags = '';
+
+console.log(`${date}, ${time} - Parsing Scss comments`);
+
+// Loop through all .scss files in directory
+recursive(__dirname + '/scss/theme', function (err, files) {
+    extractComments(files); // Files is an array of filenames
+});
+
+// Extract comments from each .scss file
+function extractComments(arr) {
+    commentTags += '[';
+
+    arr.map(function(file, index) {
+        var fileContent = fs.readFileSync(file, 'utf8'),
+            stringFileContent = JSON.stringify(parse(fileContent)), // Parse comments to JSON
+            trimmedToObject = stringFileContent.slice(1, -1); // Trim [] off JSON
+
+        // Only separate with comma if it's not the last one
+        index !== arr.length - 1 ? commentTags += trimmedToObject + ',' : commentTags += trimmedToObject;
+    });
+
+    commentTags += ']';
+
+    // Write commentTags to a file
+    fs.writeFile(__dirname + '/js/commentJson/test.json', commentTags, function(err) {
+        if (err) {
+            return console.log(err);
+        } else {
+            return console.log('Successfully wrote file.');
+        }
+    });
+}
