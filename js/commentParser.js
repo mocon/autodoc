83a065d67871bc98eb89f49de3db7fe3535e63a0
myles <myles@Myles-OConnor-iMac-27.local>
2016-08-09 17:00:56 -0700

    Formatting JSON records

--- js/commentParser.js
+++ js/commentParser.js
@@ -8,13 +8,21 @@ var parse = require('comment-parser'),
 
 console.log(`${date}, ${time} - Parsing Scss comments`);
 
+// TODO: Loop through all .scss files in directory
+
+// Parse comments to JSON
 test = JSON.stringify(parse(content));
 
+// Trim [] off JSON
+console.log(test.slice(1, -1));
+
+// TODO: Concatenate JSON objects together, comma separated, and enclose in []
+
 // Write test to a file
-fs.writeFile(__dirname + '/commentJson/test.json', test, function(err) {
-    if (err) {
-        return console.log(err);
-    } else {
-        return console.log('Successfully wrote file.');
-    }
-});
+// fs.writeFile(__dirname + '/commentJson/test.json', test, function(err) {
+//     if (err) {
+//         return console.log(err);
+//     } else {
+//         return console.log('Successfully wrote file.');
+//     }
+// });
