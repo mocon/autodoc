7808531be36697c9ca9cedbccf0998933c128a3c
myles <myles@Myles-OConnor-iMac-27.local>
2016-08-11 11:52:02 -0700

    Progress on React rendered docs

--- docs/app.js
+++ docs/app.js
@@ -13,7 +13,7 @@ var json = (function () {
     return json;
 })();
 
-// Main app component
+// <App /> component
 var App = React.createClass({
     getInitialState: function() {
         // Use generated JSON file as state
@@ -23,10 +23,87 @@ var App = React.createClass({
     },
     render: function() {
         return (
-            <div className="gds-text--bold">
-                Hello, world!
+            <div>
+                <GdsPageHeader />
+                <div className="gds-layout__container">
+                    <div className="gds-layout__row">
+                        <div className="gds-layout__column--lg-9 gds-layout__column--md-12">
+                            <MainSection components={this.state.json} />
+                        </div>
+                        <div className="gds-layout__column--lg-3 gds-layout__hidden-md-down">
+                            <Sidebar components={this.state.json} />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
+    }
+});
+
+// <MainSection /> component
+var MainSection = React.createClass({
+    render: function() {
+        var components = this.props.components;
+
+        return (
+            <section>
+                <ul>
+                    {components.forEach(function(component) {
+                        console.log(component.description);
+
+                        return (
+                            <MainSectionItem component={component} />
+                        )
+                    })}
+                </ul>
+            </section>
+        )
+    }
+});
+
+// <MainSectionItem /> component
+var MainSectionItem = React.createClass({
+    render: function() {
+        var component = this.props.component;
+
+        return (
+            <li>
+                <p>{component.description}</p>
+            </li>
+        )
+    }
+});
+
+// <Sidebar /> component
+var Sidebar = React.createClass({
+    render: function() {
+        return (
+            <aside>
+                <p>Sidebar</p>
+            </aside>
+        )
+    }
+});
+
+// <GdsPageHeader /> component
+var GdsPageHeader = React.createClass({
+    render: function() {
+        var headerStyle = {pointerEvents: 'none'};
+
+        return (
+            <header className="gds-page-header" style={headerStyle}>
+                <div className="gds-page-header__product-bar">
+                    <h6 className="gds-page-header__product-name">Product Name</h6>
+                    <img className="gds-page-header__logo" src="https://ds.gumgum.com/images/svg/logo-white.svg" />
+                </div>
+                <div className="gds-page-header__nav-bar">
+                    <div className="gds-page-header__primary-nav" id="gg-slide-nav-button">
+                        <h4 className="gds-page-header__page-name gds-text__header--small">Documentation</h4>
+                    </div>
+                    <div className="-clear-both"></div>
+                </div>
+            </header>
+        )
     }
 });
 
