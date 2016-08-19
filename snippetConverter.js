f5752ccb6462a64b96fbf2c124882a2bbd33b480
Myles O'Connor <myles@jarvis.local>
2016-08-18 20:21:48 -0700

    Only generate snippets for components that have a tabTrigger tag

--- snippetConverter.js
+++ snippetConverter.js
@@ -43,7 +43,7 @@ module.exports = {
     _convert: function(json) {
         console.log('Converting code snippets...');
 
-        // Loop through all components, passed into _convert() as json
+        // Loop through all components, passed into convert() as json
         json.map(function(component) {
             var defaultTextWithTabStop = '',
                 numberOfDefaultClasses = component.defaultClasses.length,
@@ -60,43 +60,46 @@ module.exports = {
                 phpStormBaseHtml = '',
                 phpStormEncodedHtml = '';
 
-            // Assemble Sublime snippet
-            sublimeSnippet += '<!-- ' + component.name + ' -->\n<snippet><content><![CDATA[\n';
-            sublimeSnippet += component.example.replace(/---]/g, '    ') + '\n';
-            sublimeSnippet += ']]></content><tabTrigger>' + component.tabTrigger + '</tabTrigger>';
-            sublimeSnippet += '<scope>text.html</scope></snippet>';
+            // If this component has a tabTrigger, create snippets for it
+            if (component.tabTrigger) {
+                // Assemble Sublime snippet
+                sublimeSnippet += '<!-- ' + component.name + ' -->\n<snippet><content><![CDATA[\n';
+                sublimeSnippet += component.example.replace(/---]/g, '    ') + '\n';
+                sublimeSnippet += ']]></content><tabTrigger>' + component.tabTrigger + '</tabTrigger>';
+                sublimeSnippet += '<scope>text.html</scope></snippet>';
 
-            // Write the Sublime Text 3 code snippet file
-            fs.writeFile('./code_snippets/sublime/' + component.tabTrigger + '.sublime-snippet', sublimeSnippet, function(err) {
-                if (err) {
-                    return console.log(err);
-                }
-            });
+                // Write the Sublime Text 3 code snippet file
+                fs.writeFile('./code_snippets/sublime/' + component.tabTrigger + '.sublime-snippet', sublimeSnippet, function(err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                });
 
-            // Assemble Vim snippet
-            vimSnippet += 'snippet ' + component.tabTrigger + ' "' + component.name + '" b' + '\n';
-            vimSnippet += component.example.replace(/---]/g, '    ') + '\n';
-            vimSnippet += 'endsnippet';
+                // Assemble Vim snippet
+                vimSnippet += 'snippet ' + component.tabTrigger + ' "' + component.name + '" b' + '\n';
+                vimSnippet += component.example.replace(/---]/g, '    ') + '\n';
+                vimSnippet += 'endsnippet';
 
-            // Write the Vim code snippet file
-            fs.writeFile('./code_snippets/vim/' + component.tabTrigger + '.snippets', vimSnippet, function(err) {
-                if (err) {
-                    return console.log(err);
-                }
-            });
+                // Write the Vim code snippet file
+                fs.writeFile('./code_snippets/vim/' + component.tabTrigger + '.snippets', vimSnippet, function(err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                });
 
-            // Assemble PhpStorm snippet
-            phpStormBaseHtml = component.example;
-            phpStormEncodedHtml = entities.encode(phpStormBaseHtml);
-            phpStormSnippet += '<template name="' + component.tabTrigger + '" value="' + phpStormEncodedHtml.replace(/---]/g, '\t').replace(/\r?\n|\r/g, '&#13;') + '" description="' + component.name + '" toReformat="false" toShortenFQNames="true">';
-            phpStormSnippet += '\n\t<context>\n\t\t<option name="HTML" value="true" />\n\t</context>\n</template>';
+                // Assemble PhpStorm snippet
+                phpStormBaseHtml = component.example;
+                phpStormEncodedHtml = entities.encode(phpStormBaseHtml);
+                phpStormSnippet += '<template name="' + component.tabTrigger + '" value="' + phpStormEncodedHtml.replace(/---]/g, '\t').replace(/\r?\n|\r/g, '&#13;') + '" description="' + component.name + '" toReformat="false" toShortenFQNames="true">';
+                phpStormSnippet += '\n\t<context>\n\t\t<option name="HTML" value="true" />\n\t</context>\n</template>';
 
-            // Write the PhpStorm code snippet file
-            fs.writeFile('./code_snippets/phpstorm/' + component.tabTrigger + '.xml', phpStormSnippet, function(err) {
-                if (err) {
-                    return console.log(err);
-                }
-            });
+                // Write the PhpStorm code snippet file
+                fs.writeFile('./code_snippets/phpstorm/' + component.tabTrigger + '.xml', phpStormSnippet, function(err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                });
+            }
 
         });
 
